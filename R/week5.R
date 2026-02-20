e9f87418a2e491807ca1548f6cd4152bd6c939d8
ross0778 <ross0778@umn.edu>
2026-02-20 12:15:20 -0600

    Import data

--- R/week5.R
+++ R/week5.R
@@ -1,3 +1,11 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+
+# Data Import
+Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+Anotes_tbl <- read_csv("../data/Anotes.csv", col_names = c("parnum", "notes"))
+Bdata_tbl <- read_delim("../data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"))
+Bnotes_tbl <- read_tsv("../data/Bnotes.txt", col_names = c("parnum", "notes"))
+
+# Data Cleaning
