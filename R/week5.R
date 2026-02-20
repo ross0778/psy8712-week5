fe893070b39e48bcebf0cc9920d57a280ead3a71
ross0778 <ross0778@umn.edu>
2026-02-20 14:57:57 -0600

    Finish importing and cleaning

--- R/week5.R
+++ R/week5.R
@@ -4,8 +4,23 @@ library(tidyverse)
 
 # Data Import
 Adata_tbl <- read_delim("../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
-Anotes_tbl <- read_csv("../data/Anotes.csv", col_names = c("parnum", "notes"))
+Anotes_tbl <- read_csv("../data/Anotes.csv", col_names = c("parnum", "notes"), col_types = cols(parnum = col_double()))
 Bdata_tbl <- read_delim("../data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"))
-Bnotes_tbl <- read_tsv("../data/Bnotes.txt", col_names = c("parnum", "notes"))
+Bnotes_tbl <- read_tsv("../data/Bnotes.txt", col_names = c("parnum", "notes"), col_types = cols(parnum = col_double()))
 
 # Data Cleaning
+Aclean_tbl <- Adata_tbl %>%
+  separate(qs, into = c("q1", "q2", "q3", "q4", "q5"), sep = "-") %>% 
+  mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>% 
+  mutate(across(starts_with("q"), as.integer)) %>% 
+ left_join(Anotes_tbl,by = "parnum") %>% 
+  filter(is.na(notes))
+ABclean_tbl <- Bdata_tbl %>%
+  mutate(datadate = as.POSIXct(datadate, format = "%b %d %Y, %H:%M:%S")) %>% 
+  mutate(across(starts_with("q"), as.integer)) %>% 
+  left_join(Bnotes_tbl, by = "parnum") %>% 
+  filter(is.na(notes)) %>% 
+  bind_rows("B" = ., "A" = Aclean_tbl, .id = "lab") %>% 
+  select(-notes)
+  
+
